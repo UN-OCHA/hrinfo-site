4383bada1ed24cec9b7edc9e016990459d1208ec
Martin Maritim <trollmatt@gmail.com>
2015-08-20 14:47:28 +0300

    #47 Fixed Calendar entries - make end date update to the start date

--- sites/all/modules/hr/hr_events/js/hr_events.js
+++ sites/all/modules/hr/hr_events/js/hr_events.js
@@ -5,6 +5,9 @@
       $('#edit-field-event-date-und-0-value-datepicker-popup-0', context).on('change', function(){ 
           $('#edit-field-event-date-und-0-value2-datepicker-popup-0').val($(this).val());
       });
+      $('#edit-field-event-date-und-0-value-timeEntry-popup-1', context).on('change', function(){ 
+          $('#edit-field-event-date-und-0-value2-timeEntry-popup-1').val($(this).val());
+      });
     }
   };
 })(jQuery);
