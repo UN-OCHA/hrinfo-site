8d38a3556842ff5d8981dc693e69ca9493e11a37
Pedro Cambra <pedro.cambra@gmail.com>
2015-09-07 17:41:20 +0100

    Issue #64 - Updated HID contact list to be private

---
+++ sites/all/modules/hid/hid_profiles/templates/hid-profiles-local-contacts-no-access.tpl.php
@@ -0,0 +1,10 @@
+<?php
+  // @TODO: Make the stuff translatable.
+?>
+<div class="contacts-list-no-access-wrapper">
+  <div class="contacts-list-no-access">
+    <div class="message">
+      <?php print $message ?>
+    </div>
+  </div>
+</div>
