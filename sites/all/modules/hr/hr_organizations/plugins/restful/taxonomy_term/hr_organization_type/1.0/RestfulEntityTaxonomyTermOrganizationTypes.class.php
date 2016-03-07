5c640c70e776bed3b1dbbcaf976dfc437d61d233
Guillaume Viguier <guillaume@viguierjust.com>
2016-03-07 11:01:25 +0100

    #368 - added a checkbox for access organization types (API)

--- sites/all/modules/hr/hr_organizations/plugins/restful/taxonomy_term/hr_organization_type/1.0/RestfulEntityTaxonomyTermOrganizationTypes.class.php
+++ sites/all/modules/hr/hr_organizations/plugins/restful/taxonomy_term/hr_organization_type/1.0/RestfulEntityTaxonomyTermOrganizationTypes.class.php
@@ -7,4 +7,18 @@
 
 class RestfulEntityTaxonomyTermOrganizationTypes extends \RestfulEntityBaseTaxonomyTerm {
 
+  /**
+   * Overrides \RestfulEntityBase::publicFieldsInfo().
+   */
+  public function publicFieldsInfo() {
+    $public_fields = parent::publicFieldsInfo();
+
+    $public_fields['humanitarian_access'] = array(
+      'property' => 'field_org_type_access',
+    );
+
+    return $public_fields;
+  }
+
+
 }
