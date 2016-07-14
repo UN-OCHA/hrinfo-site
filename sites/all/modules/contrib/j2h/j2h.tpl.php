ce5a9afa3f210d27730e73096c35f88de9d8b1d2
Peter Droogmans <peter@attiks.com>
2016-07-14 10:25:22 +0200

    Fix j2h

--- sites/all/modules/contrib/j2h/j2h.tpl.php
+++ sites/all/modules/contrib/j2h/j2h.tpl.php
@@ -1,5 +1,4 @@
 <?php
-if (isset($variables['render']) && ($variables['render'] == 'grid' || $variables['render'] == 'html')) {
-  include 'j2h_'.$variables['render'].'.tpl.php';
+if (isset($variables['render']) && ($variables['render'] == 'grid' || $variables['render'] == 'html')) {
+  include 'j2h_' . $variables['render'] . '.tpl.php';
 }
-?>
