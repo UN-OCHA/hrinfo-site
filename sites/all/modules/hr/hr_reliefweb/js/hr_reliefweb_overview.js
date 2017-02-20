4962c5dc12ff77c6e117bf720f19c0f54127ae3c
Peter Droogmans <peter@attiks.com>
2017-02-20 13:38:03 +0100

    HRINFO-579: Render as drop downs

---
+++ sites/all/modules/hr/hr_reliefweb/js/hr_reliefweb_overview.js
@@ -0,0 +1,53 @@
+(function ($, Drupal) {
+  'use strict';
+
+  $(document).ready(function () {
+    if (!document.querySelector('.reliefweb--facets')) {
+      return;
+    }
+
+    // Redirect to selected option.
+    var handleSelect = function (e) {
+      if (e.target.value) {
+        window.location = e.target.value;
+      }
+    };
+
+    var details = document.querySelectorAll('.reliefweb--facets details');
+    for (var i = 0; i < details.length; i++) {
+      var detail = details[i];
+
+      // Construct label.
+      var newLabel = document.createElement('label');
+      newLabel.innerText = detail.querySelector('summary').innerText;
+
+      // Construct select.
+      var newSelect = document.createElement('select');
+      var options = detail.querySelectorAll('li a');
+
+      // Add empty option.
+      var newOption = document.createElement('option');
+      newOption.value = '';
+      newOption.text = Drupal.t('Select a filter');
+      newSelect.appendChild(newOption);
+
+      // Add options.
+      for (var j = 0; j < options.length; j++) {
+        var option = options[j];
+        var newOption = document.createElement('option');
+        newOption.value = option.href;
+        newOption.text = option.innerText;
+        newSelect.appendChild(newOption);
+      }
+
+      // Hide details.
+      detail.classList.add('processed');
+      detail.parentNode.appendChild(newLabel);
+      detail.parentNode.appendChild(newSelect);
+    }
+
+    // Listen for change events.
+    var details = document.querySelector('.reliefweb--facets').addEventListener("change", handleSelect);
+
+  });
+})(jQuery, Drupal);
