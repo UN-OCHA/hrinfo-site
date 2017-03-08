919e9379253c67c8fdd1ef8156c527bb8b6f79c5
Guillaume Viguier <guillaume@viguierjust.com>
2017-03-08 09:41:51 +0100

    Initial work on integration of assessments

---
+++ sites/all/modules/ar/ar_assessments/templates/ar-assessments.tpl.php
@@ -0,0 +1,27 @@
+<div id="assessments-view">
+</div>
+<div id="assessments-list">
+  <table id="assessments-list-table" class="table table-striped">
+    <thead>
+    <tr>
+      <th>Title</th>
+      <th>Location(s)</th>
+      <th>Organization(s)</th>
+      <th>Participating Organization(s)</th>
+      <th>Cluster(s)/Sector(s)</th>
+      <th>Status</th>
+      <th>Assessment Date(s)</th>
+      <th>Data</th>
+    </tr>
+    </thead>
+    <tbody>
+    </tbody>
+  </table>
+  <div class="text-center">
+    <ul class="pagination">
+      <li class="pager-previous"><a id="previous" href="">Previous</a></li>
+      <li class="pager-next"> <a id="next" href="">Next</a></li>
+    </ul>
+  </div>
+</div>
+<div id="loading"><p class="text-center lead"><i class="fa fa-spinner fa-pulse"></i> Please wait. Data is loading...</p></div>
