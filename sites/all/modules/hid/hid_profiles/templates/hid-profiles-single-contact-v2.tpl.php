cc7e93addef636e82befc99187e78c597ff20e81
Guillaume Viguier <guillaume@viguierjust.com>
2017-03-27 12:06:31 +0200

    HRINFO-602 - upgrade of HID v2 block

---
+++ sites/all/modules/hid/hid_profiles/templates/hid-profiles-single-contact-v2.tpl.php
@@ -0,0 +1,33 @@
+<?php
+  /**
+   * @file: template for displaying contact information from HID.
+   */
+?>
+
+<article class="hid-contact" itemscope itemtype="https://schema.org/Person">
+  <span itemprop="name">
+    <strong>
+      <?php empty($url) ? print l("$contact->name", $url) : print "$contact->name"; ?>
+    </strong>
+  </span>
+
+  <div itemscope itemtype="https://schema.org/Organization">
+    <span itemprop="name" > <?php print $contact->organization->name; ?> </span>
+  </div>
+  <div itemprop="jobTitle">
+    <span><?php print $contact->job_title; ?></span>
+  </div>
+  <div itemprop="email">
+    <span><?php print $contact->email; ?></span>
+  </div>
+  <?php
+    if (!empty($contact->phone_number)) {
+    ?>
+        <div itemprop="contactPoint" itemscope itemtype="http://schema.org/ContactPoint">
+          <span itemprop="telephone"><?php print $contact->phone_number; ?></span>
+          (<span itemprop="contactType"><?php print $contact->phone_number_type ?></span>)
+        </div>
+    <?php
+    }
+    ?>
+</article>
