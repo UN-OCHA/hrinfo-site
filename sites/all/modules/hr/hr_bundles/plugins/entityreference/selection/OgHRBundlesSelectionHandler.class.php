06d7ea7d99db6b2219d4922ddfdefb32e64d3be2
Peter Droogmans <peter@attiks.com>
2017-10-18 09:51:08 +0200

    HRINFO-775: WIP getLabel

--- sites/all/modules/hr/hr_bundles/plugins/entityreference/selection/OgHRBundlesSelectionHandler.class.php
+++ sites/all/modules/hr/hr_bundles/plugins/entityreference/selection/OgHRBundlesSelectionHandler.class.php
@@ -48,11 +48,13 @@ class OgHRBundlesSelectionHandler extends OgSelectionHandler {
    */
   public function getLabel($entity) {
     $target_type = $this->field['settings']['target_type'];
+    return entity_label($target_type, $entity);
+
+    $target_type = $this->field['settings']['target_type'];
     $field = $entity->{OG_AUDIENCE_FIELD};
     $gid = $field[LANGUAGE_NONE][0]['target_id'];
     $group = entity_load_single('node', $gid);
     return entity_label($target_type, $entity).' ('.entity_label('node', $group).')';
   }
-
 
 }
