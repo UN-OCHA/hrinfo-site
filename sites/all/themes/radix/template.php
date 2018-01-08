1fab816135c74651b22ae5beb8fa4e5322ea45a0
www-data <www-data@vps379045.ovh.net>
2018-01-08 13:43:58 +0100

    Updated radix-7.x-3.6 views_bulk_operations-7.x-3.4 media-7.x-2.14 entitycache-7.x-1.5 search_api_solr_overrides-7.x-1.0 piwik-7.x-2.10 file_entity-7.x-2.14 views-7.x-3.18 redis-7.x-3.17 menu_target-7.x-1.6 l10n_update-7.x-2.2 languagefield-7.x-1.7 linkchecker-7.x-1.3 entityreference_autocomplete-7.x-1.13 search_api_saved_searches-7.x-1.6 hierarchical_select-7.x-3.0-beta8 views_pdf-7.x-1.7 media_youtube-7.x-3.6 defaultconfig-7.x-1.0-alpha11 advagg-7.x-2.30 varnish-7.x-1.8 search_api-7.x-1.22 feeds-7.x-2.0-beta4 wysiwyg_filter-7.x-1.6-rc9 auto_entitylabel-7.x-1.4 feeds_tamper-7.x-1.2 i18n-7.x-1.22 date-7.x-2.10 entityreference_prepopulate-7.x-1.7 field_collection-7.x-1.0-beta12 js-7.x-2.4 ctools-7.x-1.12 securelogin-7.x-1.7

--- sites/all/themes/radix/template.php
+++ sites/all/themes/radix/template.php
@@ -4,21 +4,32 @@
  * Theme hooks for Radix.
  */
 
-require_once dirname(__FILE__) . '/includes/utilities.inc';
-require_once dirname(__FILE__) . '/includes/theme.inc';
-require_once dirname(__FILE__) . '/includes/structure.inc';
-require_once dirname(__FILE__) . '/includes/form.inc';
-require_once dirname(__FILE__) . '/includes/menu.inc';
-require_once dirname(__FILE__) . '/includes/comment.inc';
-require_once dirname(__FILE__) . '/includes/panel.inc';
-require_once dirname(__FILE__) . '/includes/view.inc';
-require_once dirname(__FILE__) . '/includes/admin.inc';
-require_once dirname(__FILE__) . '/includes/contrib.inc';
+// Include all files from the includes directory.
+$includes_path = dirname(__FILE__) . '/includes/*.inc';
+foreach (glob($includes_path) as $filename) {
+  require_once dirname(__FILE__) . '/includes/' . basename($filename);
+}
 
 /**
- * Implementation of template_preprocess_html().
+ * Implements template_preprocess_html().
  */
 function radix_preprocess_html(&$variables) {
+  global $base_url;
+
+//  // Add Bootstrap JS from CDN if bootstrap library is not installed.
+  if (!module_exists('bootstrap_library')) {
+    $base = parse_url($base_url);
+    $url = $base['scheme'] . '://maxcdn.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js';
+    $jquery_ui_library = drupal_get_library('system', 'ui');
+    $jquery_ui_js = reset($jquery_ui_library['js']);
+    drupal_add_js($url, array(
+      'type' => 'external',
+      // We have to put Bootstrap after jQuery, but before jQuery UI.
+      'group' => JS_LIBRARY,
+      'weight' => $jquery_ui_js['weight'] - 1,
+    ));
+  }
+
   // Add meta for Bootstrap Responsive.
   // <meta name="viewport" content="width=device-width, initial-scale=1.0">
   $element = array(
@@ -29,12 +40,30 @@ function radix_preprocess_html(&$variables) {
     ),
   );
   drupal_add_html_head($element, 'bootstrap_responsive');
+
+  // Add some custom classes for panels pages.
+  if (module_exists('page_manager') && count(page_manager_get_current_page())) {
+    $variables['is_panel'] = TRUE;
+
+    // Get the current panel display and add some classes to body.
+    if ($display = panels_get_current_page_display()) {
+      $variables['classes_array'][] = 'panel-layout-' . $display->layout;
+
+      // Add a custom class for each region that has content.
+      $regions = array_keys($display->panels);
+      foreach ($regions as $region) {
+        $variables['classes_array'][] = 'panel-region-' . $region;
+      }
+    }
+  }
 }
 
 /**
  * Implements hook_css_alter().
  */
 function radix_css_alter(&$css) {
+  $active_theme = variable_get('theme_default', '');
+
   // Unset some panopoly css.
   if (module_exists('panopoly_admin')) {
     $panopoly_admin_path = drupal_get_path('module', 'panopoly_admin');
@@ -52,6 +81,20 @@ function radix_css_alter(&$css) {
 
   // Unset some core css.
   unset($css['modules/system/system.menus.css']);
+
+  // Remove radix stylesheets if it is not the default theme.
+  if ($active_theme != 'radix') {
+    unset($css[drupal_get_path('theme', 'radix') . '/assets/css/radix.style.css']);
+  }
+
+  // Allow themes to set preprocess to FALSE.
+  // Enable the ability to toggle <link> as opposed to <style> @import.
+  // Useful for injecting CSS.
+  $preprocess_css = variable_get('preprocess_css', 0);
+  foreach ($css as $key => $value) {
+    $css[$key]['preprocess'] = $preprocess_css;
+  }
+
 }
 
 /**
@@ -61,7 +104,7 @@ function radix_js_alter(&$javascript) {
   // Add radix-modal when required.
   if (module_exists('ctools')) {
     $ctools_modal = drupal_get_path('module', 'ctools') . '/js/modal.js';
-    $radix_modal = drupal_get_path('theme', 'radix') . '/assets/javascripts/radix-modal.js';
+    $radix_modal = drupal_get_path('theme', 'radix') . '/assets/js/radix.modal.js';
     if (!empty($javascript[$ctools_modal]) && empty($javascript[$radix_modal])) {
       $javascript[$radix_modal] = array_merge(
         drupal_js_defaults(), array('group' => JS_THEME, 'data' => $radix_modal));
@@ -71,7 +114,7 @@ function radix_js_alter(&$javascript) {
   // Add radix-field-slideshow when required.
   if (module_exists('field_slideshow')) {
     $field_slideshow = drupal_get_path('module', 'field_slideshow') . '/field_slideshow.js';
-    $radix_field_slideshow = drupal_get_path('theme', 'radix') . '/assets/javascripts/radix-field-slideshow.js';
+    $radix_field_slideshow = drupal_get_path('theme', 'radix') . '/assets/js/radix.slideshow.js';
     if (!empty($javascript[$field_slideshow]) && empty($javascript[$radix_field_slideshow])) {
       $javascript[$radix_field_slideshow] = array_merge(
         drupal_js_defaults(), array('group' => JS_THEME, 'data' => $radix_field_slideshow));
@@ -80,7 +123,7 @@ function radix_js_alter(&$javascript) {
 
   // Add radix-progress when required.
   $progress = 'misc/progress.js';
-  $radix_progress = drupal_get_path('theme', 'radix') . '/assets/javascripts/radix-progress.js';
+  $radix_progress = drupal_get_path('theme', 'radix') . '/assets/js/radix.progress.js';
   if (!empty($javascript[$progress]) && empty($javascript[$radix_progress])) {
     $javascript[$radix_progress] = array_merge(
       drupal_js_defaults(), array('group' => JS_THEME, 'data' => $radix_progress));
@@ -91,17 +134,6 @@ function radix_js_alter(&$javascript) {
  * Implements template_preprocess_page().
  */
 function radix_preprocess_page(&$variables) {
-  global $base_url;
-
-  // Add Bootstrap JS from CDN if bootstrap library is not installed.
-  if (!module_exists('bootstrap_library')) {
-    $base = parse_url($base_url);
-    drupal_add_js($base['scheme'] . '://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js', 'external');
-  }
-
-  // Add CSS for Font Awesome
-  // drupal_add_css('//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css', 'external');
-
   // Determine if the page is rendered using panels.
   $variables['is_panel'] = FALSE;
   if (module_exists('page_manager') && count(page_manager_get_current_page())) {
@@ -114,15 +146,20 @@ function radix_preprocess_page(&$variables) {
   }
 
   // Theme action links as buttons.
-  foreach ($variables['action_links'] as $key => &$link) {
-    $link['#link']['localized_options']['attributes'] = array('class' => array('btn', 'btn-primary', 'btn-sm'));
+  if (!empty($variables['action_links'])) {
+    foreach (element_children($variables['action_links']) as $key) {
+      $variables['action_links'][$key]['#link']['localized_options']['attributes'] = array(
+        'class' => array('btn', 'btn-primary', 'btn-sm'),
+      );
+    }
   }
 
   // Add search_form to theme.
   $variables['search_form'] = '';
   if (module_exists('search') && user_access('search content')) {
     $search_box_form = drupal_get_form('search_form');
-    $search_box_form['basic']['keys']['#title'] = '';
+    $search_box_form['basic']['keys']['#title'] = 'Search';
+    $search_box_form['basic']['keys']['#title_display'] = 'invisible';
     $search_box_form['basic']['keys']['#size'] = 20;
     $search_box_form['basic']['keys']['#attributes'] = array('placeholder' => 'Search');
     $search_box_form['basic']['keys']['#attributes']['class'][] = 'form-control';
@@ -134,26 +171,23 @@ function radix_preprocess_page(&$variables) {
   }
 
   // Format and add main menu to theme.
-  $variables['main_menu'] = menu_tree(variable_get('menu_main_links_source', 'main-menu'));
-  $variables['main_menu']['#theme_wrappers'] = array('menu_tree__navbar_nav');
-
-  // Format and add user menu to theme.
-  $variables['user_menu'] = menu_tree('user-menu');
-  $variables['user_menu']['#theme_wrappers'] = array('menu_tree__navbar_right');
+  $main_menu_parameters = array('min_depth' => 1);
+  $main_menu_max_depth = (int)theme_get_setting('main_menu_max_depth');
+  if ($main_menu_max_depth > 0) {
+    $main_menu_parameters['max_depth'] = $main_menu_max_depth;
+  }
+  elseif ($main_menu_max_depth == 0) {
+    // If the user upgraded from an old version, the value will be zero and so
+    // we set it to the default.
+    $main_menu_parameters['max_depth'] = 2;
+  }
+  $active_trail_items = menu_get_active_trail();
+  foreach($active_trail_items as $key => $item) {
+    $active_trail[$key] = isset($item['mlid']) ? $item['mlid'] : 0;
+  }
+  $main_menu_parameters['active_trail'] = $active_trail;
+  $variables['main_menu'] = _radix_dropdown_menu_tree(variable_get('menu_main_links_source', 'main-menu'), $main_menu_parameters);
 
   // Add a copyright message.
   $variables['copyright'] = t('Drupal is a registered trademark of Dries Buytaert.');
-
-  // Display a message if Sass has not been compiled.
-  $theme_path = drupal_get_path('theme',$GLOBALS['theme']);
-  $stylesheet_path = $theme_path . '/assets/stylesheets/screen.css';
-  if (_radix_current_theme() == 'radix') {
-    $stylesheet_path = $theme_path . '/assets/stylesheets/radix-style.css';
-  }
-  if (!file_exists($stylesheet_path)) {
-    drupal_set_message(t('It looks like %path has not been created yet. Run <code>@command</code> in your theme directory to create it.', array(
-      '%path' => $stylesheet_path,
-      '@command' => 'compass watch',
-    )), 'error');
-  }
 }
