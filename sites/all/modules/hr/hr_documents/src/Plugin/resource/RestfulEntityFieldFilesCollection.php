93e6773de9510062f161e688d822e8bd4413fc85
Guillaume Viguier <guillaume@viguierjust.com>
2018-03-30 10:43:43 +0200

    Added file collection

---
+++ sites/all/modules/hr/hr_documents/src/Plugin/resource/RestfulEntityFieldFilesCollection.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\hr_documents\Plugin\resource;
+use Drupal\restful\Plugin\resource\ResourceEntity;
+use Drupal\restful\Plugin\resource\ResourceInterface;
+
+/**
+ * Class RestfulEntityFieldFilesCollection
+ * @package Drupal\hr_documents\Plugin\resource
+ *
+ * @Resource(
+ *   name = "files_collection:1.0",
+ *   resource = "files_collection",
+ *   label = "File Collections",
+ *   description = "File Collections",
+ *   authenticationTypes = {
+ *     "hid_token"
+ *   },
+ *   authenticationOptional = TRUE,
+ *   dataProvider = {
+ *     "entityType": "field_collection",
+ *     "bundles": {
+ *       "field_files_collection"
+ *     },
+ *   },
+ *   majorVersion = 1,
+ *   minorVersion = 0
+ * )
+ */
+
+class RestfulEntityFieldFilesCollection extends ResourceEntity implements ResourceInterface {
+
+  /**
+   * Overrides \RestfulEntityBase::publicFields().
+   */
+  public function publicFields() {
+    $public_fields = parent::publicFields();
+
+    $public_fields['file'] = array(
+      'property' => 'field_file',
+    );
+
+    $public_fields['language'] = array(
+      'property' => 'field_language',
+    );
+
+    return $public_fields;
+  }
+
+}
