bdc497504eb28239c51fe68fd38940ffb88baa38
Guillaume Viguier <guillaume@viguierjust.com>
2018-05-01 12:50:51 +0200

    Fixed range parameter

--- sites/all/modules/hr/hr_assessments/src/Plugin/resource/RestfulEntityNodeAssessments.php
+++ sites/all/modules/hr/hr_assessments/src/Plugin/resource/RestfulEntityNodeAssessments.php
@@ -22,6 +22,7 @@ use Drupal\restful\Plugin\resource\ResourceInterface;
  *     "bundles": {
  *       "hr_assessment"
  *     },
+ *     "range" = 100,
  *   },
  *   majorVersion = 1,
  *   minorVersion = 0,
--- sites/all/modules/hr/hr_assessments/src/Plugin/resource/RestfulEntityTaxonomyTermPopulationTypes.php
+++ sites/all/modules/hr/hr_assessments/src/Plugin/resource/RestfulEntityTaxonomyTermPopulationTypes.php
@@ -22,6 +22,7 @@ use Drupal\restful\Plugin\resource\ResourceInterface;
  *     "bundles": {
  *       "hr_population_type"
  *     },
+ *     "range" = 100
  *   },
  *   majorVersion = 1,
  *   minorVersion = 0,
