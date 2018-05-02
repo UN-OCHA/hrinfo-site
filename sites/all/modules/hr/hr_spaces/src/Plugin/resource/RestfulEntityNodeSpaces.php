cc9f9214a737fc64c61db2d497daf975adb4ec40
Guillaume Viguier <guillaume@viguierjust.com>
2018-05-02 11:35:17 +0200

    Fix filter by parent for locations

--- sites/all/modules/hr/hr_spaces/src/Plugin/resource/RestfulEntityNodeSpaces.php
+++ sites/all/modules/hr/hr_spaces/src/Plugin/resource/RestfulEntityNodeSpaces.php
@@ -22,7 +22,7 @@ use Drupal\restful\Plugin\resource\ResourceInterface;
  *     "bundles": {
  *       "hr_space"
  *     },
- *     range = "100",
+ *     "range" = 100,
  *   },
  *   majorVersion = 1,
  *   minorVersion = 0,
