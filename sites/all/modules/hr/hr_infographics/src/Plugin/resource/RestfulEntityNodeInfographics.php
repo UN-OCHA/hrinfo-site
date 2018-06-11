b5e4209b061405cb5e185d6f69edeafe0d39b126
Guillaume Viguier <guillaume@viguierjust.com>
2018-06-11 13:42:00 +0200

    HRINFO-804 - display infographics preview

--- sites/all/modules/hr/hr_infographics/src/Plugin/resource/RestfulEntityNodeInfographics.php
+++ sites/all/modules/hr/hr_infographics/src/Plugin/resource/RestfulEntityNodeInfographics.php
@@ -206,6 +206,7 @@ class RestfulEntityNodeInfographics extends ResourceCustom implements ResourceIn
         $tmp->file->filename = $field_file['filename'];
         $tmp->file->filesize = $field_file['filesize'];
         $tmp->file->url = file_create_url($field_file['uri']);
+        $tmp->file->preview = file_create_url(_pdfpreview_create_preview($tmp->file));
         if (!empty($value->field_language)) {
           $tmp->language = $value->field_language[LANGUAGE_NONE][0]['value'];
         }
