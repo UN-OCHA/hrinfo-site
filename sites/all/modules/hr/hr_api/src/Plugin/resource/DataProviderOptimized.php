4b4db3afb86ba44a16376c884474cc4dcb42bdab
Guillaume Viguier <guillaume@viguierjust.com>
2018-07-04 16:01:18 +0200

    HRINFO-804 - fix node author is anonymous

--- sites/all/modules/hr/hr_api/src/Plugin/resource/DataProviderOptimized.php
+++ sites/all/modules/hr/hr_api/src/Plugin/resource/DataProviderOptimized.php
@@ -13,9 +13,50 @@ use Drupal\restful\Http\RequestInterface;
 
 class DataProviderOptimized  extends DataProviderEntity implements DataProviderInterface {
   /**
+   * Overrides DataProviderEntity::getQueryForList().
+   *
+   * Expose only published nodes.
+   */
+  public function getQueryForList() {
+    $query = parent::getQueryForList();
+    if ($this->entityType === 'node') {
+      $query->propertyCondition('status', NODE_PUBLISHED);
+    }
+    return $query;
+  }
+
+  /**
+   * Overrides DataProviderEntity::getQueryCount().
+   *
+   * Only count published nodes.
+   */
+  public function getQueryCount() {
+    $query = parent::getQueryCount();
+    if ($this->entityType === 'node') {
+      $query->propertyCondition('status', NODE_PUBLISHED);
+    }
+    return $query;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function discover($path = NULL) {
     return array();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function entityPreSave(\EntityDrupalWrapper $wrapper) {
+    if ($this->entityType === 'node') {
+      $node = $wrapper->value();
+      if (!empty($node->nid)) {
+        // Node is already saved.
+        return;
+      }
+      node_object_prepare($node);
+      $node->uid = $this->getAccount()->uid;
+    }
   }
 }
