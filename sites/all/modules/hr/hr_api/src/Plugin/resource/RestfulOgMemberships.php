9c02548e29216e17803fdd26eaa7f9fa924ae79b
Guillaume Viguier <guillaume@viguierjust.com>
2018-08-14 11:38:51 +0200

    HRINFO-804 - added og memberships to API

---
+++ sites/all/modules/hr/hr_api/src/Plugin/resource/RestfulOgMemberships.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\hr_api\Plugin\resource\RestfulOgMemberships.
+ */
+
+namespace Drupal\hr_api\Plugin\resource;
+
+use Drupal\hr_api\Plugin\resource\ResourceCustom;
+use Drupal\restful\Plugin\resource\ResourceInterface;
+
+/**
+ * Class RestfulOgMemberships
+ * @package Drupal\hr_api\Plugin\resource
+ *
+ * @Resource(
+ *   name = "og_memberhips:1.0",
+ *   resource = "og_membership",
+ *   label = "OG Membership",
+ *   description = "Export the OG Memberships.",
+ *   authenticationOptional = TRUE,
+ *   authenticationTypes = {
+ *     "hid_token"
+ *   },
+ *   dataProvider = {
+ *     "entityType": "og_memberhips",
+ *   },
+ *   majorVersion = 1,
+ *   minorVersion = 0,
+ *   allowOrigin = "*"
+ * )
+ */
+class RestfulOgMemberships extends ResourceCustom implements ResourceInterface {
+
+}
