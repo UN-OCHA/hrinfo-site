0231fb04a7f17ed3679cc090aefd67cac6649274
Guillaume Viguier <guillaume@viguierjust.com>
2018-08-22 11:35:25 +0200

    HRINFO-804 - group permissions via API

--- sites/all/modules/hr/hr_api/src/Plugin/resource/DataProvider/DataProviderOgMemberships.php
+++ sites/all/modules/hr/hr_api/src/Plugin/resource/DataProvider/DataProviderOgMemberships.php
@@ -9,6 +9,7 @@ namespace Drupal\hr_api\Plugin\resource\DataProvider;
 
 use Drupal\restful\Exception\ForbiddenException;
 use Drupal\restful\Plugin\resource\DataProvider\DataProviderEntity;
+use Drupal\restful\Http\RequestInterface;
 
 /**
  * Class DataProviderOgMemberships.
@@ -21,30 +22,20 @@ class DataProviderOgMemberships extends DataProviderEntity {
    * {@inheritdoc}
    */
   public function create($object) {
-    $this->validateBody($object);
-    $entity_info = $this->getEntityInfo();
-    $bundle_key = $entity_info['entity keys']['bundle'];
-    // TODO: figure out how to derive the bundle when posting to a resource with
-    // multiple bundles.
-    $bundle = reset($this->bundles);
-    $values = $bundle_key ? array($bundle_key => $bundle) : array();
 
-    $entity = entity_create($this->entityType, $values);
+    $og_membership = og_membership_create('node', $object['group'], 'user', hid_profiles_get_uid_by_hid($object['entity']), 'og_user_node');
 
-    if ($this->checkEntityAccess('create', $this->entityType, $entity) === FALSE) {
+    if ($this->checkEntityAccess('create', $this->entityType, $og_membership) === FALSE) {
       // User does not have access to create entity.
       throw new ForbiddenException('You do not have access to create a new resource.');
     }
 
-    /* @var \EntityDrupalWrapper $wrapper */
-    $wrapper = entity_metadata_wrapper($this->entityType, $entity);
-
-    $this->setPropertyValues($wrapper, $object, TRUE);
+    $og_membership->save();
 
     // The access calls use the request method. Fake the view to be a GET.
     $old_request = $this->getRequest();
     $this->getRequest()->setMethod(RequestInterface::METHOD_GET);
-    $output = array($this->view($wrapper->getIdentifier()));
+    $output = array($this->view($og_membership->identifier()));
     // Put the original request back to a POST.
     $this->request = $old_request;
 
