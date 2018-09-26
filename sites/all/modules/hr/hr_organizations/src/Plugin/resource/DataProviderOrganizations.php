85fc9670b675e49dd468ad7da8938fc897b56c43
Guillaume Viguier <guillaume@viguierjust.com>
2018-09-26 10:10:01 +0200

    HRINFO-804 - Translations via API

--- sites/all/modules/hr/hr_organizations/src/Plugin/resource/DataProviderOrganizations.php
+++ sites/all/modules/hr/hr_organizations/src/Plugin/resource/DataProviderOrganizations.php
@@ -31,4 +31,51 @@ class DataProviderOrganizations  extends DataProviderEntity implements DataProvi
     $query->addTag('hr_organizations_acronym');
   }
 
+  /**
+   * {@inheritdoc}
+   */
+  public function update($identifier, $object, $replace = FALSE) {
+    global $language;
+    $this->validateBody($object);
+    $entity_id = $this->getEntityIdByFieldId($identifier);
+    $this->isValidEntity('update', $entity_id);
+
+    $term = taxonomy_term_load($entity_id);
+    $handler  = entity_translation_get_handler('taxonomy_term', $term);
+    $translations = $handler->getTranslations();
+    $languages = array_keys($translations->data);
+    if (!in_array($language->language, $languages)) {
+      // Create a translation.
+      $translation = array(
+        'translate' => 0,
+        'status' => 1,
+        'language' => $language->language,
+        'source' => $translations->original,
+      );
+      $handler->setTranslation($translation);
+    }
+
+    /* @var \EntityDrupalWrapper $wrapper */
+    $wrapper = entity_metadata_wrapper($this->entityType, $entity_id);
+    $wrapper_translated = $wrapper->language($language->language);
+
+    $this->setPropertyValues($wrapper_translated, $object, $replace);
+
+    // Set the HTTP headers.
+    $this->setHttpHeader('Status', 201);
+
+    if (!empty($wrapper_translated->url) && $url = $wrapper_translated->url->value()) {
+      $this->setHttpHeader('Location', $url);
+    }
+
+    // The access calls use the request method. Fake the view to be a GET.
+    $old_request = $this->getRequest();
+    $this->getRequest()->setMethod(RequestInterface::METHOD_GET);
+    $output = array($this->view($identifier));
+    // Put the original request back to a PUT/PATCH.
+    $this->request = $old_request;
+
+    return $output;
+  }
+
 }
