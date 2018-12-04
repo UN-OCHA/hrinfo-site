7b1422843fccd5f782ee7cb32b5a0f2870df0d33
Guillaume Viguier <guillaume@viguierjust.com>
2018-12-04 17:20:52 +0100

    HRINFO-879 - fix permission issues

--- sites/all/modules/hr/hr_api/src/Plugin/resource/DataProviderOptimized.php
+++ sites/all/modules/hr/hr_api/src/Plugin/resource/DataProviderOptimized.php
@@ -64,6 +64,40 @@ class DataProviderOptimized  extends DataProviderEntity implements DataProviderI
   /**
    * {@inheritdoc}
    */
+  public function create($object) {
+    $this->validateBody($object);
+    $entity_info = $this->getEntityInfo();
+    $bundle_key = $entity_info['entity keys']['bundle'];
+    // TODO: figure out how to derive the bundle when posting to a resource with
+    // multiple bundles.
+    $bundle = reset($this->bundles);
+    $values = $bundle_key ? array($bundle_key => $bundle) : array();
+
+    $entity = entity_create($this->entityType, $values);
+
+    /* @var \EntityDrupalWrapper $wrapper */
+    $wrapper = entity_metadata_wrapper($this->entityType, $entity);
+
+    $this->setPropertyValues($wrapper, $object, TRUE);
+
+    if ($this->checkEntityAccess('create', $this->entityType, $entity) === FALSE) {
+      // User does not have access to create entity.
+      throw new ForbiddenException('You do not have access to create a new resource.');
+    }
+
+    // The access calls use the request method. Fake the view to be a GET.
+    $old_request = $this->getRequest();
+    $this->getRequest()->setMethod(RequestInterface::METHOD_GET);
+    $output = array($this->view($wrapper->getIdentifier()));
+    // Put the original request back to a POST.
+    $this->request = $old_request;
+
+    return $output;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function update($identifier, $object, $replace = FALSE) {
     global $language;
     $this->validateBody($object);
