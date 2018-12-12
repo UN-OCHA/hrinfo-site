788cb8105e44215c8bba7e0ca2fcffc4e45c7fc4
left23 <lcdemc@gmail.com>
2018-12-12 17:30:52 +0100

    preprocess and includes, a mix of old and new, enabled as needed

--- sites/all/themes/ocha_basic/template.php
+++ sites/all/themes/ocha_basic/template.php
@@ -5,6 +5,13 @@
  * Template overrides, preprocess, and alter hooks for the OCHA Basic theme.
  */
 
+require_once dirname(__FILE__) . '/includes/structure.inc';
+require_once dirname(__FILE__) . '/includes/menu.inc';
+require_once dirname(__FILE__) . '/includes/node.inc';
+require_once dirname(__FILE__) . '/includes/panel.inc';
+require_once dirname(__FILE__) . '/includes/user.inc';
+require_once dirname(__FILE__) . '/includes/view.inc';
+
 /**
  * Implements hook_form_alter().
  */
@@ -100,6 +107,12 @@ function ocha_basic_preprocess_html(&$vars) {
   drupal_add_html_head($fav_32, 'favicon-32x32');
   drupal_add_html_head($fav_16, 'favicon-16x16');
   drupal_add_html_head($safari_pinned_tab, 'safari_pinned_tab');
+
+  if ($node = menu_get_object()) {
+    if (og_is_group('node', $node->nid)) {
+      $variables['classes_array'][] = 'hr-group-context';
+    }
+  }
 }
 
 /**
@@ -147,12 +160,73 @@ function ocha_basic_preprocess_page(&$vars) {
   $vars['page']['language_switcher'] = $output;
 
 
-//  // Set variable based on path alias to include inline-search.
-//  $path = drupal_get_path_alias();
-//  if ($path == 'inline-search') {
-//    $inline_search = TRUE;
-//    $vars['inline_search'] = $inline_search;
-//  }
+  global $theme_path;
+
+  $variables['hr_tabs'] = array();
+  $header_img_path = $theme_path.'/img/headers/general.png';
+  if (module_exists('og_context')) {
+    $gid = og_context_determine_context('node');
+    if (!empty($gid)) {
+      $og_group = entity_load('node', array($gid));
+      $og_group = $og_group[$gid];
+      if ($og_group->type == 'hr_operation') {
+        // Salahumanitaria logo
+        if ($og_group->nid == 77) { // Nid of the Colombia operation
+          $variables['logo'] = '/sites/all/themes/ocha_basic/img/logos/salahumanitaria_logo.png';
+        }
+        if (!empty($og_group->field_operation_type) && !empty($og_group->field_operation_region) && $og_group->field_operation_type[LANGUAGE_NONE][0]['value'] == 'country') {
+          // Determine the region of the operation
+          $region_id = $og_group->field_operation_region[LANGUAGE_NONE][0]['target_id'];
+          $region = entity_load_single('node', $region_id);
+          $region_uri = entity_uri('node', $region);
+          $region_status = $region->field_operation_status[LANGUAGE_NONE][0]['value'];
+          switch ($region_status) {
+            case 'active':
+              // Add the region to the tabs
+              $variables['hr_tabs'][] = l($region->title, $region_uri['path'], $region_uri['options']);
+              break;
+            case 'inactive':
+              break;
+            case 'archived':
+              break;
+          }
+        }
+      }
+      elseif ($og_group->type == 'hr_disaster') {
+        $glide = $og_group->field_glide_number[LANGUAGE_NONE][0]['value'];
+        if ($glide == 'EP-2014-000041-GIN') {
+          $variables['logo'] = '/sites/all/themes/ocha_basic/img/logos/unmeer_logo.png';
+        }
+      }
+      elseif ($og_group->type == 'hr_bundle') {
+        // Get operation from bundle
+        $op_gid = _hr_bundles_get_operation($og_group->nid);
+        if (!empty($op_gid)) {
+          $operation = entity_load_single('node', $op_gid);
+          $op_uri = entity_uri('node', $operation);
+          $variables['hr_tabs'][] = l($operation->title, $op_uri['path'], $op_uri['options']);
+        }
+      }
+      $uri = entity_uri('node', $og_group);
+      if ($og_group->status) { // Group is published
+        $variables['hr_tabs'][] = l($og_group->title, $uri['path'], $uri['options']);
+      }
+      else {
+        $variables['hr_tabs'][] = '<a href="#">'.$og_group->title.'</a>';
+      }
+      $group_img_path = '/img/headers/'.$og_group->type.'/'.strtolower(str_replace(array(' ','/'), '-', $og_group->title)).'.png';
+      if (file_exists(dirname(__FILE__).$group_img_path)) {
+        $header_img_path = $theme_path.$group_img_path;
+      }
+    }
+  }
+
+  $variables['og_group_header_image'] = theme('image', array(
+    'path' => $header_img_path,
+    'alt' => 'Header image',
+  ));
+
+  $variables['hr_favorite_spaces'] = _ocha_basic_block_render('hr_bookmarks', 'hr_favorite_spaces');
 
 }
 
@@ -185,41 +259,41 @@ function ocha_basic_pwa_manifest_alter(&$manifest) {
 // Bootstrap Dropdown menu.
 // from https://github.com/drupalprojects/bootstrap/blob/7.x-3.x/templates/menu/menu-link.func.php.
 // See https://www.drupalgeeks.com/drupal-blog/how-render-bootstrap-sub-menus for second level dropdown.
-function ocha_basic_menu_link(array $variables) {
-  $element = $variables['element'];
-  $sub_menu = '';
-  $options = !empty($element['#localized_options']) ? $element['#localized_options'] : array();
-  // Check plain title if "html" is not set, otherwise, filter for XSS attacks.
-  $title = empty($options['html']) ? check_plain($element['#title']) : filter_xss_admin($element['#title']);
-  // Ensure "html" is now enabled so l() doesn't double encode. This is now
-  // safe to do since both check_plain() and filter_xss_admin() encode HTML
-  // entities. See: https://www.drupal.org/node/2854978
-  $options['html'] = TRUE;
-  $href = $element['#href'];
-  $attributes = !empty($element['#attributes']) ? $element['#attributes'] : array();
-  if ($element['#below']) {
-    // Prevent dropdown functions from being added to management menu so it
-    // does not affect the navbar module.
-    if (($element['#original_link']['menu_name'] == 'management') && (module_exists('navbar'))) {
-      $sub_menu = drupal_render($element['#below']);
-    }
-    elseif ((!empty($element['#original_link']['depth'])) && ($element['#original_link']['depth'] == 1)) {
-      // Add our own wrapper.
-      unset($element['#below']['#theme_wrappers']);
-      $sub_menu = '<ul class="menu">' . drupal_render($element['#below']) . '</ul>';
-      // Generate as standard dropdown.
-      $title .= '<svg class="icon icon--arrow-down"><use xlink:href="#arrow-down"></use></svg>';
-      // Set dropdown trigger element to # to prevent inadvertant page loading
-      // when a submenu link is clicked.
-      $options['attributes']['data-target'] = '#';
-      $options['attributes']['data-toggle'] = 'dropdown';
-      $options['attributes']['aria-expanded'] = 'false';
-      $options['attributes']['aria-haspopup'] = 'true';
-    }
-  }
-
-  return '<li' . drupal_attributes($attributes) . '>' . l($title, $href, $options) . $sub_menu . "</li>\n";
-}
+//function ocha_basic_menu_link(array $variables) {
+//  $element = $variables['element'];
+//  $sub_menu = '';
+//  $options = !empty($element['#localized_options']) ? $element['#localized_options'] : array();
+//  // Check plain title if "html" is not set, otherwise, filter for XSS attacks.
+//  $title = empty($options['html']) ? check_plain($element['#title']) : filter_xss_admin($element['#title']);
+//  // Ensure "html" is now enabled so l() doesn't double encode. This is now
+//  // safe to do since both check_plain() and filter_xss_admin() encode HTML
+//  // entities. See: https://www.drupal.org/node/2854978
+//  $options['html'] = TRUE;
+//  $href = $element['#href'];
+//  $attributes = !empty($element['#attributes']) ? $element['#attributes'] : array();
+//  if ($element['#below']) {
+//    // Prevent dropdown functions from being added to management menu so it
+//    // does not affect the navbar module.
+//    if (($element['#original_link']['menu_name'] == 'management') && (module_exists('navbar'))) {
+//      $sub_menu = drupal_render($element['#below']);
+//    }
+//    elseif ((!empty($element['#original_link']['depth'])) && ($element['#original_link']['depth'] == 1)) {
+//      // Add our own wrapper.
+//      unset($element['#below']['#theme_wrappers']);
+//      $sub_menu = '<ul class="menu">' . drupal_render($element['#below']) . '</ul>';
+//      // Generate as standard dropdown.
+//      $title .= '<svg class="icon icon--arrow-down"><use xlink:href="#arrow-down"></use></svg>';
+//      // Set dropdown trigger element to # to prevent inadvertant page loading
+//      // when a submenu link is clicked.
+//      $options['attributes']['data-target'] = '#';
+//      $options['attributes']['data-toggle'] = 'dropdown';
+//      $options['attributes']['aria-expanded'] = 'false';
+//      $options['attributes']['aria-haspopup'] = 'true';
+//    }
+//  }
+//
+//  return '<li' . drupal_attributes($attributes) . '>' . l($title, $href, $options) . $sub_menu . "</li>\n";
+//}
 
 
 function ocha_basic_menu_tree__user_menu(&$variables) {
@@ -229,4 +303,26 @@ function ocha_basic_menu_tree__user_menu(&$variables) {
   $before .= '</span><svg class="icon icon--arrow-down"><use xlink:href="#arrow-down"></use></svg></button><ul class="cd-global-header__dropdown cd-dropdown cd-user-menu__dropdown" role="menu">';
   $after = '</ul>';
   return $before.$variables['tree'].$after;
+}
+
+
+/**
+ * Custom function to render a block so I can manually position it in the markup
+ */
+function _ocha_basic_block_render($module, $block_id) {
+  $block = block_load($module, $block_id);
+  $block_content = _block_render_blocks(array($block));
+  $build = _block_get_renderable_array($block_content);
+  $block_rendered = drupal_render($build);
+  return $block_rendered;
+}
+
+/**
+ * Implements hook_preprocess_fieldable_panels_pane()
+ * Fixes title appearing 2 times in fieldable panels panes
+ */
+function ocha_basic_preprocess_fieldable_panels_pane(&$variables) {
+  if (isset($variables['content']['title'])) {
+    unset($variables['content']['title']);
+  }
 }
