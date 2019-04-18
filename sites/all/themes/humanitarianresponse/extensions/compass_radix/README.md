cb557c1b2a6571729d4721c6a8c57cd27bddfbda
Guillaume Viguier <guillaume@viguierjust.com>
2019-04-18 09:53:29 +0200

    Removed humanitarianresponse theme - Replaced by ocha_basic

--- sites/all/themes/humanitarianresponse/extensions/compass_radix/README.md
+++
@@ -1,6 +0,0 @@
-Compass Radix
-=============
-
-A Compass extension for Drupal's Radix theme.
-
-For documentation and more see [http://drupal.org/project/radix](http://drupal.org/project/radix).
--- sites/all/themes/humanitarianresponse/extensions/compass_radix/templates/project/manifest.rb
+++
@@ -1,7 +0,0 @@
-stylesheet '_compass_radix.scss', :media => 'screen, projection'
-
-description "A Compass plugin for the Drupal Radix theme"
-
-help %Q{
-See http://drupal.org/project/radix for documentation and help.
-}
