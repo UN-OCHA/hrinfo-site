2904db39edddbee3c5e3314a03c47912d774ca30
Peter Lieverdink <me@cafuego.net>
2019-08-08 17:08:12 +1000

    [OPS-5807] PHPCS the custom modules and fix their code comments.

--- html/sites/all/modules/custom/j2h/j2h.tpl.php
+++ html/sites/all/modules/custom/j2h/j2h.tpl.php
@@ -1,4 +1,10 @@
 <?php
+
+/**
+ * @file
+ * j2h.tpl.php
+ */
+
 if (isset($variables['render']) && ($variables['render'] == 'grid' || $variables['render'] == 'html')) {
   include 'j2h_' . $variables['render'] . '.tpl.php';
 }
