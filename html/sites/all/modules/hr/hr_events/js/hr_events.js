d86262ce464efdde018c9aec07562937d5fb26d5
Andy Footner <andyfootner@netscape.net>
2019-10-17 11:15:13 +0200

    [HRINFO-977] first pass at events form

--- html/sites/all/modules/hr/hr_events/js/hr_events.js
+++ html/sites/all/modules/hr/hr_events/js/hr_events.js
@@ -2,6 +2,7 @@
 (function($) {
   Drupal.behaviors.hrEventsDatePopupValue = {
     attach: function (context, settings) {
+      // TODO: only bump date2 when it's before date1.
       $('#edit-field-event-date-und-0-value-datepicker-popup-0', context).on('change', function(){ 
           $('#edit-field-event-date-und-0-value2-datepicker-popup-0').val($(this).val());
       });
---
+++ html/sites/all/modules/hr/hr_events/js/hr_events_toggle_fields.js
@@ -0,0 +1,82 @@
+/**
+       * @file
+       * Custom script for toggling hidden fields.
+*/
+
+(function ($, Drupal) {
+  'use strict;'
+  Drupal.behaviors.hrEventsToggleFields = {
+    attach: function (context, settings) {
+
+      var toggleTextShow = {
+        en: 'Show more',
+        es: 'Ver más',
+        fr: 'Afficher plus',
+        ru: 'Показать больше'
+      };
+      var toggleTextHide = {
+        en: 'Hide extra fields',
+        es: 'Ocultar campos extra',
+        fr: 'Masquer les champs supplémentaires',
+        ru: 'Скрыть дополнительные'
+      };
+      var lang = document.getElementsByTagName('html')[0].getAttribute('lang');
+      /**
+       * Create a button element.
+       */
+      function createButton(value, label, disabled) {
+        console.log('creating button');
+        var button = document.createElement('button');
+        button.setAttribute('type', 'button');
+        button.setAttribute('value', value);
+        button.appendChild(document.createTextNode(label));
+        button.disabled = disabled === true;
+        return button;
+      }
+
+      /**
+       * Respond to clicks on 'add' button. Reveal another input and remove
+       * button if appropriate.
+       */
+      function toggleFields() {
+        console.log('clicked');
+        var toggle = this.previousElementSibling;
+        if (this.value === 'show') {
+          if (document.documentElement.clientWidth > 1024) {
+            toggle.style.display = 'flex';
+          }
+          else {
+            toggle.style.display = 'block';
+          }
+          this.value = 'hide';
+          this.innerHTML = toggleTextHide[lang];
+        }
+        else {
+          toggle.style.display = 'none';
+          this.value = 'show';
+          this.innerHTML = toggleTextShow[lang];
+        }
+      }
+
+      /**
+       * Hide 'extra' inputs and add button to toggle them.
+       */
+      function prepareToggle() {
+        console.log('preparing');
+        var additional = document.getElementsByClassName('hr-additional')[0];
+        console.log('additional', additional);
+        if (additional !== undefined && additional.children.length > 0) {
+          additional.children[0].style.display = 'none';
+          // We only need one per table.
+          if (additional.getElementsByClassName('hr-toggle-button').length === 0) {
+            var add = createButton('show', toggleTextShow[lang]);
+            add.className = 'hr-toggle-button btn btn-default';
+            add.addEventListener('click', toggleFields);
+            additional.appendChild(add);
+          }
+        }
+      }
+      prepareToggle();
+    }
+  };
+}(jQuery, Drupal));
