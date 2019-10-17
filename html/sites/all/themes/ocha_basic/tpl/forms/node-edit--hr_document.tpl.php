35fc8840bb07098ae490c48eaac1f774d440c19a
Andy Footner <andyfootner@netscape.net>
2019-10-17 15:29:45 +0200

    [HRINFO-973] alter field order

--- html/sites/all/themes/ocha_basic/tpl/forms/node-edit--hr_document.tpl.php
+++ html/sites/all/themes/ocha_basic/tpl/forms/node-edit--hr_document.tpl.php
@@ -9,34 +9,31 @@
 <?php print drupal_render($form['title_field']); ?>
 <?php print drupal_render($form['language']); ?>
 <fieldset class="main-inputs">
+  <?php print drupal_render($form['field_document_type']); ?>
   <?php print drupal_render($form['body']); ?>
   <?php print drupal_render($form['field_files_collection']); ?>
 </fieldset>
 <fieldset class="details">
-  <?php print drupal_render($form['field_exclude_reliefweb']); ?>
+  <?php print drupal_render($form['og_group_ref']); ?>
   <?php print drupal_render($form['field_sectors']); ?>
   <?php print drupal_render($form['field_publication_date']); ?>
-  <?php print drupal_render($form['field_document_type']); ?>
   <?php print drupal_render($form['field_organizations']); ?>
-  <?php print drupal_render($form['field_disasters']); ?>
-</fieldset>
-<fieldset class="main-inputs">
-  <?php print drupal_render($form['og_group_ref']); ?>
-</fieldset>
-<fieldset class="details">
   <?php print drupal_render($form['field_coordination_hubs']); ?>
+  <?php print drupal_render($form['field_disasters']); ?>
   <?php print drupal_render($form['field_bundles']); ?>
 </fieldset>
 
 <fieldset class="hr-additional">
   <div class="toggle">
-    <?php print drupal_render($form['field_locations']); ?>
+    <?php print drupal_render($form['field_related_content']); ?>
     <fieldset class="hr-additional-side">
+      <?php print drupal_render($form['field_locations']); ?>
       <?php print drupal_render($form['field_themes']); ?>
-      <?php print drupal_render($form['field_related_content']); ?>
     </fieldset>
   </div>
 </fieldset>
+
+<?php print drupal_render($form['field_exclude_reliefweb']); ?>
 
 <fieldset id="actions">
   <?php print drupal_render($form['actions']); ?>
