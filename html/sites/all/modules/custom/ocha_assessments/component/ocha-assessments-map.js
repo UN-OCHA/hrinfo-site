1aff63857d6028aaa71ec4a0145329e93d407c24
Peter Droogmans (attiks) <peter@attiks.com>
2020-06-15 12:57:55 +0200

    Remove debug

--- html/sites/all/modules/custom/ocha_assessments/component/ocha-assessments-map.js
+++ html/sites/all/modules/custom/ocha_assessments/component/ocha-assessments-map.js
@@ -241,7 +241,7 @@ const createMarker = () => document.createComment('');
  *    * (") then any non-("), or
  *    * (') then any non-(')
  */
-const lastAttributeNameRegex = 
+const lastAttributeNameRegex =
 // eslint-disable-next-line no-control-regex
 /([ \x09\x0a\x0c\x0d])([^\0-\x1F\x7F-\x9F "'>=/]+)([ \x09\x0a\x0c\x0d]*=[ \x09\x0a\x0c\x0d]*(?:[^ \x09\x0a\x0c\x0d"'`<>=]*|"[^"]*|'[^']*))$/;
 
@@ -2298,7 +2298,7 @@ class LitElement extends UpdatingElement {
             // The last item is kept to try to preserve the cascade order with the
             // assumption that it's most important that last added styles override
             // previous styles.
-            const addStyles = (styles, set) => styles.reduceRight((set, s) => 
+            const addStyles = (styles, set) => styles.reduceRight((set, s) =>
             // Note: On IE set.add() does not return the set
             Array.isArray(s) ? addStyles(s, set) : (set.add(s), set), set);
             // Array.from does not work on Set in IE, otherwise return
@@ -3175,16 +3175,99 @@ const typography = css`
     font-size: var(--cd-font-size-base);
     font-family: var(--cd-font);
     color: var(--cd-dark-grey);
+    line-height: 1.5;
   }
 `;
 
 const buttonStyles = css`
-  .blue-button {
-    color: white;
-    background-color: blue;
+  a {
+    color: var(--cd-dark-blue);
+   }
+
+  a:hover,
+  a:focus {
+    color: var(--cd-ocha-blue);
+    text-decoration: none;
   }
-  .blue-button:disabled {
-    background-color: grey;
+
+  .cd-button {
+    -webkit-appearance: none;
+    border-radius: 0;
+    box-shadow: none;
+    border: 0;
+    padding: 0.5rem 1rem;
+    font-size: 1rem;
+    transition: background 0.3s ease;
+    width: auto;
+  }
+
+  .cd-button--small {
+    padding: 0.25rem 0.5rem;
+    font-size: 0.8rem;
+    font-weight: 400;
+  }
+
+  .cd-button--icon {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .cd-button:hover,
+  .cd-button:focus {
+    background-color: rbga(0, 0 ,0, 0.1);
+  }
+
+  .cd-button:focus {
+    outline: 3px solid var(--cd-ocha-blue);
+  }
+
+  .cd-button--style {
+    background: var(--cd-ocha-blue);
+    color: var(--cd-white);
+  }
+
+  .cd-button--style:hover,
+  .cd-button--style:focus {
+    background: var(--cd-dark-blue);
+    color: var(--cd-white);
+  }
+
+  .cd-button--bold {
+    font-weight: bold;
+  }
+
+  .cd-button--uppercase {
+    text-transform: uppercase;
+  }
+
+  /* Some buttons have SVG icons */
+  .cd-button--icon svg {
+    fill: var(--cd-white);
+    width: 2rem;
+    height: 2rem;
+    /* Icon before */
+    padding: 0 0.5rem 0 0;
+  }
+
+  .cd-button--icon span + svg {
+    /* Icon after */
+    padding: 0 0 0 0.5rem;
+  }
+
+  .cd-button--icon:hover svg,
+  .cd-button--icon:focus svg {
+    fill: var(--cd-white);
+  }
+
+  .cd-button--export {
+    background: var(--cd-mid-grey);
+    color: var(--cd-white);
+  }
+
+  .cd-button--export:hover,
+  .cd-button--export:focus {
+    background: var(--cd-dark-grey);
   }
 `;
 
@@ -3192,23 +3275,122 @@ const dropdownStyles = css`
   .dropdown {
     min-width: 10em;
   }
+
+  .cd-filters {
+    margin-bottom: 1rem;
+  }
+
+  .cd-filters__form {
+    display: block;
+    background: var(--cd-light-grey);
+    padding: 1rem;
+  }
+
+  .cd-filters__form form{
+    margin-bottom: 0;
+  }
+
+  .cd-filters__form label {
+    display: block;
+    margin: 0 0 0.25rem;
+  }
+
+  .cd-filters__group {
+    margin: 0 0 1rem;
+  }
+
+  .cd-filters__form select {
+    width: 100%;
+    max-width: 100%;
+  }
+
+  .cd-filters__form .cd-button {
+    text-transform: unset;
+    font-weight: normal;
+  }
+
+  @media (min-width: 768px) {
+    .cd-filters {
+      margin-bottom: 2rem;
+    }
+
+    .cd-filters__form form {
+      display: flex;
+      flex-wrap: wrap;
+      align-items: flex-start;
+    }
+
+    .cd-filters__group {
+      padding-right: 1rem;
+    }
+
+    .cd-filters__form .cd-button {
+      align-self: flex-end;
+      margin-bottom: 1rem;
+    }
+
+    /* Quantity queries */
+    /* https://alistapart.com/article/quantity-queries-for-css */
+
+    /* If one element */
+    .cd-filters__group:only-of-type,
+    /* If two elements */
+    .cd-filters__group:nth-last-child(2):first-child,
+    .cd-filters__group:nth-last-child(2):first-child ~ .cd-filters__group,
+    .cd-filters__group:nth-last-child(2):first-child ~ .cd-export,
+    /* If three elements */
+    .cd-filters__group:nth-last-child(3):first-child,
+    .cd-filters__group:nth-last-child(3):first-child ~ .cd-filters__group,
+    .cd-filters__group:nth-last-child(3):first-child ~ .cd-export {
+      flex: 1 0 33%;
+      max-width: 33%;
+    }
+
+    .cd-filters__group:nth-last-child(2):first-child ~ .cd-filters__group:last-child,
+    .cd-filters__group:nth-last-child(3):first-child ~ .cd-filters__group:last-child {
+      padding-right: 0;
+    }
+
+    /* If four elements */
+    .cd-filters__group:nth-last-child(4):first-child,
+    .cd-filters__group:nth-last-child(4):first-child ~ .cd-filters__group,
+    .cd-filters__group:nth-last-child(4):first-child ~ .cd-export {
+      flex: 1 0 25%;
+      max-width: 25%;
+    }
+
+    .cd-filters__group:nth-last-child(4):first-child ~ .cd-filters__group:last-child {
+      padding-right: 0;
+    }
+
+    /* If five elements */
+    .cd-filters__group:nth-last-child(5):first-child,
+    .cd-filters__group:nth-last-child(5):first-child ~ .cd-filters__group,
+    .cd-filters__group:nth-last-child(5):first-child ~ .cd-export {
+      flex: 1 0 20%;
+      max-width: 20%;
+    }
+
+    .cd-filters__group:nth-last-child(5):first-child ~ .cd-filters__group:last-child {
+      padding-right: 0;
+    }
+  }
 `;
 
 const tableStyles = css`
-  .cd-table {
+ .cd-table {
     margin: 0 auto 3rem;
     border-collapse: collapse;
     width: 100%;
-    empty-cells: hide;
   }
 
-  th,
-  td {
+  .cd-table th,
+  .cd-table td {
     padding: 0.5rem;
     text-align: left;
   }
 
-  th {
+  .cd-table th {
     color: var(--cd-ocha-blue);
     border-bottom: 1px solid white;
     background: var(--cd-site-bg-color);
@@ -3219,14 +3401,14 @@ const tableStyles = css`
   }
 
   @media (min-width: 576px) {
-    th[data-sort-type="numeric"],
-    .cd-table--amount,
-    .cd-table--amount-total {
+    .cd-table th[data-sort-type="numeric"],
+    .cd-table .cd-table--amount,
+    .cd-table .cd-table--amount-total {
       text-align: right;
     }
   }
 
-  tfoot {
+  .cd-table tfoot {
     font-weight: bold;
   }
 
@@ -3257,47 +3439,41 @@ const tableStyles = css`
 
   @media (max-width: 575px) {
     /* Force table to not be like tables anymore */
-    table,
-    thead,
-    tbody,
-    tfoot,
-    th,
-    td,
-    tr {
+    .cd-table--responsive,
+    .cd-table--responsive thead,
+    .cd-table--responsive tbody,
+    .cd-table--responsive tfoot,
+    .cd-table--responsive th,
+    .cd-table--responsive td,
+    .cd-table--responsive tr {
       display: block;
     }
 
     /* Hide table headers (but not display: none;, for accessibility) */
-    thead tr {
+    .cd-table--responsive thead tr {
       position: absolute;
       top: -9999px;
       left: -9999px;
     }
 
-    tr {
+    .cd-table--responsive tr {
       border-bottom: 1px solid var(--cd-light-grey);
       padding: 0 !important;
     }
 
-    td {
+    .cd-table--responsive td {
       /* Behave  like a "row" */
       border: none;
       border-bottom: 1px solid var(--cd-site-bg-color);
       position: relative;
       padding: 0.5rem;
       padding-left: 40% !important;
-      min-height: 2rem;
+      min-height: 2rem; /* label should wrap */
       white-space: normal !important;
       text-align: left;
     }
 
-    td:empty {
-      border-bottom: none;
-      padding: 0;
-      min-height: unset;
-    }
-
-    td:before {
+    .cd-table--responsive td:before {
       position: absolute;
       top: 0.5rem;
       left: 0.5rem;
@@ -3311,8 +3487,8 @@ const tableStyles = css`
       content: attr(data-content);
     }
 
-    td:empty:before {
-      content: none;
+    .cd-table--responsive tfoot td {
+      border-bottom: 0 none;
     }
 
     .cd-table--row-numbers tbody tr td.cd-table--row-num {
@@ -3323,10 +3499,67 @@ const tableStyles = css`
       font-weight: bold;
       font-size: 1.5rem;
     }
+  }
 
-    tfoot td {
-      border-bottom: 0 none;
+  .cd-list {
+    margin: 0;
+    padding: 0 0 1rem;
+    list-style: none;
+  }
+
+  .cd-list li {
+    padding-bottom: 1rem;
+    margin-bottom: 1rem;
+    border-bottom: 1px solid var(--cd-site-bg-color);
+  }
+
+  .cd-list li p:last-child {
+    margin-bottom: 0;
+  }
+
+  .cd-list__title {
+    margin: 0 0 0.5rem;
+  }
+`;
+
+const paginationStyles = css`
+  .cd-pager {
+    clear: both;
+    text-align: center;
+    padding: 0.5rem;
+  }
+
+  @supports (display: grid) {
+    .cd-pager {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: center;
     }
+  }
+
+  @supports (display: grid) {
+    .cd-pager__item {
+      display: flex;
+      align-self: baseline;
+    }
+  }
+
+  .cd-pager__item {
+    padding: 0.5rem 0.75rem;
+   }
+
+  .cd-pager button {
+    display: block;
+    background-color: white;
+    border: 1px solid var(--cd-site-bg-color);
+    border-radius: 0.25rem;
+   }
+
+  .cd-pager button:hover,
+  .cd-pager button:focus {
+    color: white;
+    background-color: var(--cd-dark-blue);
+    border-color: var(--cd-dark-blue);
   }
 `;
 
@@ -3403,8 +3636,6 @@ class OchaAssessmentsBase extends LitElement {
         this.disabledFilters.push(parts[0]);
       }
     });
-
-    console.log(this.disabledFilters);
   }
 
   updated(changedProperties) {
@@ -3492,13 +3723,17 @@ class OchaAssessmentsBase extends LitElement {
     let dropdowns = this.buildFacets();
 
     return html`
-      <div class="filters">
-        ${
-          dropdowns.map(
-            d => this.renderDropdown(d)
-          )
-        }
-        <button @click="${this.resetData}">Reset</button>
+      <div class="cd-filters">
+        <div class="cd-filters__form">
+          <form>
+            ${
+              dropdowns.map(
+                d => this.renderDropdown(d)
+              )
+            }
+            <button @click="${this.resetData}" class="cd-button cd-button--style cd-button--small">Reset</button>
+          </form>
+        </div>
       </div>
     `;
   }
@@ -3523,7 +3758,7 @@ class OchaAssessmentsBase extends LitElement {
     }
 
     return html`
-      <div class="filter">
+      <div class="cd-filters__group">
         <label for="${dropdown.label}">${dropdown.label}</label>
         <high-select class="dropdown" search arrow animated @change="${this.changeSrc}" id="${dropdown.id}">
           <high-option value="${emptytOption.value}">${emptytOption.label}</high-option>
@@ -3587,13 +3822,13 @@ class OchaAssessmentsBase extends LitElement {
     }
 
     return html`
-      <div class="pager">
+      <div class="cd-pager">
         ${this.pager.current_page > 0?
-          html`<button class="pager-prev" @click="${this.prevPage}">Previous</button>`: html``
+          html`<button class="cd-pager--prev cd-pager__item" @click="${this.prevPage}">Previous</button>`: html``
         }
-        <span><span class="page-num">${this.pager.current_page + 1}</span> / <span class="page-total">${this.pager.total_pages}</span></span>
+        <span class="cd-pager__item"><span class="page-num">${this.pager.current_page + 1}</span> / <span class="page-total">${this.pager.total_pages}</span></span>
         ${this.pager.current_page < this.pager.total_pages - 1?
-          html`<button class="pager-next" @click="${this.nextPage}">Next</button>`: html``
+          html`<button class="cd-pager--next cd-pager__item" @click="${this.nextPage}">Next</button>`: html``
         }
       </div>
     `;
@@ -19000,8 +19235,9 @@ class OchaAssessmentsMap extends OchaAssessmentsBase {
     return [
       super.styles,
       css`
-        :host { display: block;
-          border: 1px solid green;
+        :host {
+          display: block;
+          border: 1px solid transparent;
         }`
     ]
   }
@@ -19018,8 +19254,6 @@ class OchaAssessmentsMap extends OchaAssessmentsBase {
           @apply (--leaflet-map-component)
         }
       </style>
-
-      <p>Source (debug): ${this.src}</p>
 
       ${this.renderErrorMessage()}
 
