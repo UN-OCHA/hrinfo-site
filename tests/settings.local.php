03bd0098a9fe60c89eb9e80aa4faf034159894e4
Peter Lieverdink <lieverdink@reliefweb.int>
2023-06-13 15:55:25 +0200

    chore: Ensure the tests can build the docker image, so they can run phpcs.
    Refs: OPS-7509

---
+++ tests/settings.local.php
@@ -0,0 +1,15 @@
+<?php
+
+$settings['file_private_path'] = '../private_files';
+
+$databases['default']['default'] = array (
+  'database' => 'drupal',
+  'username' => 'drupal',
+  'password' => 'drupal',
+  'prefix' => '',
+  'host' => '127.0.0.1',
+  'port' => '',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+
